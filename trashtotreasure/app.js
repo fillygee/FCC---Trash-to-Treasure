548fe670de0da64163b1ffb536f2705c830637f5
DuncanCaplan <duncan.caplan@outlook.com>
2023-08-21 18:06:27 +0100

    Fixed register POST issue

--- trashtotreasure/app.js
+++ trashtotreasure/app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const posts = require('./routes/post');
-const users = require('./routes/users');
-const home = require('./routes/home');
-const filter = require('./middleware/filter');
-const logger = require('./middleware/logRoutes');
+const posts = require("./routes/post");
+const users = require("./routes/users");
+const home = require("./routes/home");
+const filter = require("./middleware/filter");
+const logger = require("./middleware/logRoutes");
 const app = express();
 
 app.use(cors());
@@ -13,10 +13,10 @@ app.use(express.json());
 // app.use(filter);
 app.use(logger);
 
-app.use(express.static('static'));
+app.use(express.static("static"));
 
-app.use('/posts', posts);
-app.use('/users', users);
-app.use('/', home);
+app.use("/posts", posts);
+app.use("/users", users);
+app.use("/", home);
 
 module.exports = app;
