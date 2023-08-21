47992e4a034278b1298cab88160d7f294e77df05
aslaterm32 <aslaterm32@gmail.com>
2023-08-21 10:41:05 +0100

    database setup added

--- trashtotreasure/db/db.js
+++ trashtotreasure/db/db.js
@@ -1,9 +1,10 @@
-require("dotenv").config();
-
-const { Pool } = require("pg");
+require('dotenv').config();
+const { Pool } = require('pg');
 
 const db = new Pool({
-  connectionString: process.env.DB_URL,
+    connectionString: process.env.DB_URL,
 });
+
+console.log('Connected to DB');
 
 module.exports = db;
